7d4595f6c4f8d18a62b40571ee22bc0bfdf61736
acckioctober <acckioctober@gmail.com>
2023-04-28 17:01:22 +0600

    perfomed

--- class1.py
+++ class1.py
@@ -1,3 +1,13 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+
+class NewestClass(Hello):
+    def __init__(self, str):
+        super().__init__(str)
+        self.str = str
+    def __str__(self):
+        return self.str
+
+
+
--- task.py
+++ task.py
@@ -1,2 +1,7 @@
-from art import tprint
-tprint("PYTHON")
+# from art import tprint
+# tprint("PYTHON")
+
+from class1 import NewestClass
+new = NewestClass("PYTHON")
+print(new)
+
